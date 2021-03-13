d5a35d835eb64d62c2b01a783755aa9b5ad266b6
Holly <71049594+HDuCoing@users.noreply.github.com>
2021-03-13 16:18:49 +1300

    Add files via upload

---
+++ Simple Language Translator.py
@@ -0,0 +1,17 @@
+animal = input("Enter 'cat' or 'dog': ")
+if (animal) == "cat":
+    language = input("What language would you like to translate to? German / French / Spanish: ")
+    if (language) == "German":
+        print("The translation of cat into German is katze")
+    elif language == "French":
+        print("The translation of cat into French is chat")
+    elif language == "Spanish":
+        print("The translation of cat into Spanish is gato")
+if (animal) == "dog":
+    language = input("What language would you like to translate to? German / French / Spanish: ")
+    if (language) == "German":
+        print("The translation of dog into German is hund")
+    elif language == "French":
+        print("The translation of dog into French is chien")
+    elif language == "Spanish":
+        print("The translation of dog into Spanish is perro")
